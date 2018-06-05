602589e2dc68df7765d72502b0fbefc1208bae81
evgene dervoedov <zhenek_dervoedov@mail.ru>
2018-06-05 17:37:07 +0300

    Создание проекта

---
+++ Our_furniture_shop/Our_furniture_shop/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Our_furniture_shop
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Our_furniture_shop/Our_furniture_shop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Our_furniture_shop.Startup))]
+namespace Our_furniture_shop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
