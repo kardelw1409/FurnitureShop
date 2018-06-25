9061cdaf3620214dde3a2b56992cf9b0d6a6450e
evgene dervoedov <zhenek_dervoedov@mail.ru>
2018-06-25 18:34:58 +0300

    fixed: Пересоздавалась таблица пользователей при перезапуске приложения

--- Shop_project/Shop_project/Global.asax.cs
+++ Shop_project/Shop_project/Global.asax.cs
@@ -14,7 +14,6 @@ namespace Shop_project
     {
         protected void Application_Start()
         {
-            Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
